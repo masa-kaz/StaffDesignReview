db78b53977d6c6b8b20d8a0da6d828dd5dd7f29e
masa-kaz <135308678+masa-kaz@users.noreply.github.com>
2025-09-24 16:06:52 +0900

    更新

---
+++ package/front/src/components/icons/IconSupport.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    <path d="M10 2a8 8 0 1 0 0 16h3a3 3 0 0 0 3-3v-1a1 1 0 1 0-2 0v1a1 1 0 0 1-1 1h-3a6 6 0 1 1 6-6 1 1 0 1 0 2 0A8 8 0 0 0 10 2z"/>
+  </svg>
+</template>
+
+
---
+++ package/front/src/components/icons/IconTooling.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    <path d="M7 2a3 3 0 0 0-3 3c0 .34.06.66.17.96L2.29 7.83a1 1 0 1 0 1.42 1.41L5.6 7.37c.3.11.62.17.96.17a3 3 0 1 0 0-6zM12.59 9l-7.3 7.3A2 2 0 0 0 7 19h9a2 2 0 0 0 2-2v-9a2 2 0 0 0-2.7-1.86L12.59 9z"/>
+  </svg>
+  
+</template>
+
+
